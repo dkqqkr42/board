6fbd6488a1802cf089487bbe61db3170b1f7208e
dkqqkr42 <dkqqkr42@naver.com>
2020-08-11 16:51:11 +0900

    123

--- board/settings.py
+++ board/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g27@rty43@x_dgj_b_8kr+x7g6ayf5iiqitcv!%rz%+lom&k0n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
